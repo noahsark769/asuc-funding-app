c0cf9ecb54f0c41a8699ccf0e6fb4fe471648c83
Noah Gilmore <noah.w.gilmore@gmail.com>
2012-11-17 15:27:49 -0800

    added more static media and templates, started to do css

--- asucfunding/asucfunding/settings.py
+++ asucfunding/asucfunding/settings.py
@@ -1,3 +1,4 @@
+import os
 # Django settings for asucfunding project.
 
 DEBUG = True
@@ -59,17 +60,18 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/static_media/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/static/'
+STATIC_URL = '/static_media/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), 'static_media/').replace('\\','/'),
 )
 
 # List of finder classes that know how to find static files in
@@ -109,7 +111,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "home/Documents/programming/django/asuc-funding-app/asucfunding/templates",
+    # "Users/noah/Documents/programming/django/asuc-funding-app/asucfunding/templates",
+    "asucfunding/templates",
 )
 
 INSTALLED_APPS = (
--- asucfunding/funding_app/views.py
+++ asucfunding/funding_app/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def home(request):
+	return render_to_response("base.html",
+		{'name':"Shitface Mcgee", 'is_admin': True},
+		context_instance=RequestContext(request))
