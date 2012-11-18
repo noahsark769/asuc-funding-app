5515e2ccc867d47b92872193bab479c037027062
Ashwin Iyengar <ash@Ashwins-MacBook-Pro.local>
2012-11-17 18:49:07 -0800

    created views, and edited models

--- asucfunding/funding_app/models.py
+++ asucfunding/funding_app/models.py
@@ -1,37 +1,57 @@
 from django.db import models
 
 class Admin (models.Model):
-    UId = models.CharField(max_length=30)
-    email = models.Charfield(max_length=30)
+    uid = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
 
 class FundingRequest (models.Model):
+    uid = models.IntegerField()
     email = models.CharField(max_length=20)
     phone = models.CharField(max_length=20)
-    type = models.CharField(max_length=20)
+    requestType = models.CharField(max_length=20)
     requestCategory = models.CharField(max_length=20)
-    grantCategory = odels.CharField(max_length=20)
+    grantCategory = models.CharField(max_length=20)
     eventType = models.CharField(max_length=20)
     fundingRound = models.CharField(max_length=20)
-    sponsoringStudentGroup = models.CharField(max_length=40)
+    studentGroup = models.CharField(max_length=40)
+    requestStatus = models.CharField(max_length=20)
     
 class GraduateRequest(FundingRequest):
-    academicDepartmentalAffilliate = models.CharField(max_length=20)
+    academicDepartmentalAffiliate = models.CharField(max_length=20)
     gaDelegate = models.CharField(max_length=20)
-    studentOrgMemTot = models.IntegerField(min_value=0, max_value=99999)
-    studentOrgGrad = models.IntegerField(min_value=0, max_value=99999)
-    studentOrgUnd = models.IntegerField(min_value=0, max_value=99999)
-    AttendedWaiver = models.BooleanField()
+    studentOrgMemTot = models.IntegerField()
+    studentOrgGrad = models.IntegerField()
+    studentOrgUnd = models.IntegerField()
+    attendedWaiver = models.BooleanField()
     
-class Undergraduate(FundingRequest):
-    studentOrgTot = models.IntegerField(min_value=0, max_value=99999)
-    stduentOrgStud = models.IntegerField(min_value=0, max_value=99999)
-    attened = models.BooleanField()
+class UndergraduateRequest(FundingRequest):
+    studentOrgTot = models.IntegerField()
+    stduentOrgStud = models.IntegerField()
+    attended = models.BooleanField()
     
-class Travel(FundingRequest):
+class TravelRequest(FundingRequest):
     requestingAs = models.CharField(max_length=20)
-    
-    
+
+class Budget(models.Model):
+    additionalInfo = models.CharField(max_length=2000)
+    amountAwarded = models.DecimalField(max_digits=10,decimal_places=2)
+    comment = models.CharField(max_length=200)
+    totalRequestedAmount = models.DecimalField(max_digits=10,decimal_places=2)
+    totalOtherFunding = models.DecimalField(max_digits=10,decimal_places=2)
+    requestedTotal = models.DecimalField(max_digits=10,decimal_places=2)
+
+class ItemDescription(models.Model):
+    budget = models.ForeignKey(Budget)
+    description = models.CharField(max_length=30)
+    comment = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    itemCost = models.DecimalField(max_digits=10,decimal_places=2)
+    requestedAmount = models.DecimalField(max_digits=10,decimal_places=2)
+    otherFunding = models.DecimalField(max_digits=10,decimal_places=2)
+    total = models.DecimalField(max_digits=10,decimal_places=2)
+
 class Event(models.Model):
+    fundingRequest = models.ForeignKey(FundingRequest)
     eventTitle = models.CharField(max_length=30)
     description = models.CharField(max_length=2000)
     sameBudgetForRecurringEvents = models.BooleanField()
@@ -39,66 +59,52 @@ class Event(models.Model):
     endDate = models.DateField('%m/%d/%y')
     location = models.CharField(max_length=30)
     waiverRequested = models.BooleanField()
-    attendenceNonStudent = models.IntegerField(min_value=0, max_value=99999)
-    attendenceUnd = models.IntegerField(min_value=0, max_value=99999)
-    attendenceGrad = models.IntegerField(min_value=0, max_value=99999)
-    budgetDetail = manyToManyField(BudgetDetail)
+    attendenceNonStudent = models.IntegerField()
+    attendenceUnd = models.IntegerField()
+    attendenceGrad = models.IntegerField()
+    budget = models.ForeignKey(Budget)
     
 class TravelEvent(Event):
     depatureDate = models.DateField('%m/%d/%y')
     returnDate = models.DateField('%m/%d/%y')
-    presenting = models.BooleanField
+    presenting = models.BooleanField()
     presentationTitle = models.CharField(max_length=30)
-
-class BudgetDetail(models.Model):
-    itemDescription = manytoManyField(ItemDescription)
-    additionalInfo = models.CharField(max_length=2000)
-    ammountAwarded = models.DecimalField(max_digits=10,decimal_places=2)
-    comment = models.CharField(max_length=200)
-
-class ItemDescription(models.Model):
-    description = models.CharField(max_length=30)
-    comment = models.CharField(max_length=100)
-    quantity = models.IntegerField(min_value=0, max_value=9999)
-    itemCost = models.DecimalField(max_digits=10,decimal_places=2)
-    requestedAmount = models.DecimalField(max_digits=10,decimal_places=2)
-    otherFunding = models.DecimalField(max_digits=10,decimal_places=2)
-    total = models.DecimalField(max_digits=10,decimal_places=2)
     
 class Config(models.Model):
+    pass
     
 class ConfigAdmin (models.Model):
     config = models.ForeignKey(Config)
-    email = models.Charfield(max_length=30)
+    email = models.CharField(max_length=30)
     
-class ConfigEvents (models.Model):
+class ConfigLocation (models.Model):
     config = models.ForeignKey(Config)
-    eventLocation = models.Charfield(max_length=30)
+    location = models.CharField(max_length=30)
 
 class ConfigUGReqCat (models.Model):
     config = models.ForeignKey(Config)
-    category = models.Charfield(max_length=30)
+    category = models.CharField(max_length=30)
     
-class ConfigGRReqCat (models.Model):
+class ConfigGradReqCat (models.Model):
     config = models.ForeignKey(Config)
-    category = models.Charfield(max_length=30) 
+    category = models.CharField(max_length=30) 
 
 class ConfigUGGrant (models.Model):
     config = models.ForeignKey(Config)
-    category = models.Charfield(max_length=30) 
+    category = models.CharField(max_length=30) 
     
-class configGradGrant (models.Model):
+class ConfigGradGrant (models.Model):
     config = models.ForeignKey(Config)
-    category = models.Charfield(max_length=30) 
+    category = models.CharField(max_length=30) 
 
-class FundinRounds (models.Model):
+class ConfigFundingRound (models.Model):
     config = models.ForeignKey(Config)
-    name = models.Charfield(max_length=30)
+    name = models.CharField(max_length=30)
     deadline = models.DateField('%m/%d/%Y')
 
-class GradDelegates (models.Model):
+class ConfigGradDelegate (models.Model):
     config = models.ForeignKey(Config)
-    name = models.Charfield(max_length=30)
-    email = models.Charfield(max_length=30)
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
 
     
