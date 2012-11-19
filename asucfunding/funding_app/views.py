3ddc7dfd325c5da5b6cc7bfbe1d5627a9afacdb1
Ashwin Iyengar <ash@Ashwins-MacBook-Pro.local>
2012-11-18 22:30:24 -0800

    removed Config object

--- asucfunding/funding_app/views.py
+++ asucfunding/funding_app/views.py
@@ -32,9 +32,8 @@ def get_credentials(request):
 	return {'uid': request.COOKIES['uid'], 'name': request.COOKIES['name'], 'phone': request.COOKIES['phone'], 'email': request.COOKIES['email']}
 
 def is_admin(request):
-	c = Config.objects.all()[0]
 	try:
-		admin = c.configadmin_set.get(email=get_credentials(request)['email'])
+		admin = ConfigAdmin.objects.get(email=get_credentials(request)['email'])
 		return True
 	except ObjectDoesNotExist as e:
 		return False
@@ -149,16 +148,14 @@ def config(request):
 
 	user = get_credentials(request)
 
-	c = Config.objects.all()[0]
-
-	admins = c.configadmin_set.all()
-	locations = c.configlocation_set.all()
-	ug_req_cats = c.configugreqcat_set.all()
-	grad_req_cats = c.configgradreqcat_set.all()
-	ug_grant_cats = c.configuggrant_set.all()
-	grad_grant_cats = c.configgradgrant_set.all()
-	funding_rounds = c.configfundinground_set.all()
-	grad_delegates = c.configgraddelegate_set.all()
+	admins = ConfigAdmin.objects.all()
+	locations = ConfigLocation.objects.all()
+	ug_req_cats = ConfigUGReqCat.objects.all()
+	grad_req_cats = ConfigGradReqCat.objects.all()
+	ug_grant_cats = ConfigUGGrant.objects.all()
+	grad_grant_cats = ConfigGradGrant.objects.all()
+	funding_rounds = ConfigFundingRound.objects.all()
+	grad_delegates = ConfigGradDelegate.objects.all()
 
 	return render_to_response("config.html",{
 		'admins': admins,
@@ -175,46 +172,45 @@ def config(request):
 def change_config(request):
 	if request.method == 'POST' and is_admin() and request.POST.get('config_key') is not None:
 		config_key = request.POST.get('config_key')
-		c = Config.objects.all()[0]
 		if config_key == 'admin_roster':
 			email = request.POST.get('admin_email')
 			if email:
-				o = ConfigAdmin.objects.create(config=c, email=email)
+				o = ConfigAdmin.objects.create(email=email)
 				return HttpResponse('<li id="admin_email_'+ str(o.id) +'">' + o.email + '</li>')
 			else:
 				return HttpResponse('FAILURE')
 		elif config_key == 'event_locations':
 			location = request.POST.get('location')
 			if location:
-				o = ConfigLocation.objects.create(config=c, location=location)
+				o = ConfigLocation.objects.create(location=location)
 				return HttpResponse('SUCCESS')
 			else:
 				return HttpResponse('FAILURE')
 		elif config_key == 'ug_request_categories':
 			cat = request.POST.get('ug_req_cat')
 			if cat:
-				o = ConfigUGReqCat.objects.create(config=c, category=cat)
+				o = ConfigUGReqCat.objects.create(category=cat)
 				return HttpResponse('SUCCESS')
 			else:
 				return HttpResponse('FAILURE')
 		elif config_key == 'grad_request_categories':
 			cat = request.POST.get('grad_req_cat')
 			if cat:
-				o = ConfigGradReqCat.objects.create(config=c, category=cat)
+				o = ConfigGradReqCat.objects.create(category=cat)
 				return HttpResponse('SUCCESS')
 			else:
 				return HttpResponse('FAILURE')
 		elif config_key == 'ug_grant_categories':
 			cat = request.POST.get('ug_grant_cat')
 			if cat:
-				o = ConfigUGGrant.objects.create(config=c, category=cat)
+				o = ConfigUGGrant.objects.create(category=cat)
 				return HttpResponse('SUCCESS')
 			else:
 				return HttpResponse('FAILURE')
 		elif config_key == 'grad_grant_categories':
 			cat = request.POST.get('grad_grant_cat')
 			if cat:
-				o = ConfigGradGrant.objects.create(config=c, category=cat)
+				o = ConfigGradGrant.objects.create(category=cat)
 				return HttpResponse('SUCCESS')
 			else:
 				return HttpResponse('FAILURE')
@@ -231,7 +227,7 @@ def change_config(request):
 					this_date = None
 
 				if this_date and name:
-					o = ConfigFundingRound.objects.create(config=c, name=name, deadline=this_date)
+					o = ConfigFundingRound.objects.create(name=name, deadline=this_date)
 					return HttpResponse('SUCCESS')
 				else:
 					return HttpResponse('FAILURE')
@@ -242,7 +238,7 @@ def change_config(request):
 			email = request.POST.get('delegate_email')
 
 			if email and name:
-				o = ConfigGradDelegate.objects.create(config=c, name=name, email=email)
+				o = ConfigGradDelegate.objects.create(name=name, email=email)
 				return HttpResponse('SUCCESS')
 			else:
 				return HttpResponse('FAILURE')
